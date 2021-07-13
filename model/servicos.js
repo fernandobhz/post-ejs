1a6957688bdb613fe4411b07f7c89f53d30fbec7
Nathalia Tescarollo <nathtescarollo@gmail.com>
2021-07-13 20:30:33 -0300

    Implementando Multer para importar listaServicos

--- model/servicos.js
+++ model/servicos.js
@@ -1,5 +1,9 @@
-const listaServicos = require("../arquivos/listaServicos.json");
+let listaServicos = require("../arquivos/listaServicos.json");
 
 exports.retornarListaServicos = () => listaServicos;
 
 exports.adicionarItemServico = ({id, desc, preco}) => listaServicos.push({id, desc, preco})
+
+exports.importarListaServicos = ( itensServico ) => {
+    listaServicos = itensServico;
+};
